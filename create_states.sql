a694ea2fff4dbbfab72cd5b749d3ac773ead6bef
GBPeters <gijstebrij@gmail.com>
2017-01-20 04:54:00 +0100

    Added sql

---
+++ create_states.sql
@@ -0,0 +1,28 @@
+﻿-- Table: public.states
+
+-- DROP TABLE public.states;
+
+CREATE TABLE public.states
+(
+  id bigint NOT NULL DEFAULT nextval('states_id_seq'::regclass),
+  response_id bigint,
+  icao24 character(6) NOT NULL,
+  callsign character varying(15),
+  origin_country character varying(50),
+  time_position bigint,
+  time_velocity bigint,
+  longitude double precision,
+  latitude double precision,
+  altitude double precision,
+  on_ground boolean,
+  velocity double precision,
+  heading double precision,
+  vertical_rate double precision,
+  CONSTRAINT states_pkey PRIMARY KEY (id),
+  CONSTRAINT states_response_id_icao24_key UNIQUE (response_id, icao24)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.states
+  OWNER TO postgres;
