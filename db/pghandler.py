d2bf24219d69507a7b32688cf00aede6ac1fc272
GBPeters <gijstebrij@gmail.com>
2017-01-31 12:03:03 +0100

    pghandler now has config class and settings, defaults to localhost.

--- db/pghandler.py
+++ db/pghandler.py
@@ -5,25 +5,45 @@ This module contains a PostgreSQL connection handler and require info.
 
 import psycopg2
 
-DB_NAME='upair'
-DB_HOST = '145.100.59.98'
-DB_USER = 'teammaja'
-DB_PASS = 'maja'
-DB_PORT='5432'
-DB_DRIVER='org.postgresql.Driver'
+DB_DRIVER = 'org.postgresql.Driver'
+
+
+class Config:
+    """
+    Configurations container
+    """
+
+    def __init__(self, name="upair", host="localhost", user="postgres", passw="postgres", port="5432"):
+        self.DB_NAME = name
+        self.DB_HOST = host
+        self.DB_USER = user
+        self.DB_PASS = passw
+        self.DB_PORT = port
+
+
+CONFIG = {
+    "CLOUD": Config(
+        host="145.100.59.98",
+        user="teammaja",
+        passw="maja",
+    ),
+    "LOCAL": Config(
+    )
+}
 
 class Connection:
     """
     Class handeling connection to a PostgreSQL database.
     """
 
-    def __init__(self, autocommit=True, hardFail=False):
+    def __init__(self, conf="LOCAL", autocommit=True, hardFail=False):
         self.autocommit = autocommit
         self.hardFail = hardFail
+        self.conf = CONFIG[conf]
         self.__connect()
 
-    def __enter__(self, autcommit=True, hardFail=False):
-        self.__init__(autcommit, hardFail)
+    def __enter__(self, conf="LOCAL", autocommit=True, hardFail=False):
+        self.__init__(conf, autocommit, hardFail)
         return self
 
     def __exit__(self, type, value, traceback):
@@ -39,7 +59,8 @@ class Connection:
             return self.hardFail
 
     def __connect(self):
-        self._connection = psycopg2.connect(database=DB_NAME, host=DB_HOST, user=DB_USER, password=DB_PASS, port=DB_PORT)
+        self._connection = psycopg2.connect(database=self.conf.DB_NAME, host=self.conf.DB_HOST, user=self.conf.DB_USER,
+                                            password=self.conf.DB_PASS, port=self.conf.DB_PORT)
         self._cursor = self._connection.cursor()
         self._copyfrom = self._cursor.copy_from
 
