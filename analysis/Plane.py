15727626e90df986256ce3a29aac1fc14f5f4432
Simon Veen <simon.veen@wur.nl>
2017-01-31 13:03:51 +0100

    anls.py created

--- analysis/Plane.py
+++ analysis/Plane.py
@@ -1,6 +1,7 @@
-from db.pghandler import Connection
-
 class Plane:
+    """
+    Class for handeling Plane data
+    """
     def __init__(self, id, icao, tailnum, country, time_pos, time_vel, lat, lon, alt, ground, vel, heading, vert):
         self.id = id
         self.icao = icao
@@ -15,30 +16,6 @@ class Plane:
         self.vel = vel
         self.heading = heading
         self.vert = vert
-
-
-with Connection() as c:
-    states = c.selectAll("SELECT * FROM states Limit 10000")
-    planes = []
-    for s in states:
-        print s
-        id = s[0]
-        icao = s[2]
-        tailnum = s[3]
-        country = s[4]
-        time_pos = s[5]
-        time_vel = s[6]
-        lon = s[7]
-        lat = s[8]
-        alt = s[9]
-        ground = s[10]
-        vel = s[11]
-        heading = s[12]
-        vert = s[13]
-        plane = Plane(id, icao, tailnum, country, time_pos, time_vel, lat, lon, alt, ground, vel, heading, vert)
-        planes += [plane]
-
-print planes[1].heading
 
 
 
